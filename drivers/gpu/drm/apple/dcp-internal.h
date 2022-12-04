f8f925b6f3c560d64e7b0eb50c279b2076471e5b
Janne Grunau <j@jannau.net>
2022-12-04 11:36:05 +0100

    drm/apple: Check if DCP firmware is supported
    Signed-off-by: Janne Grunau <j@jannau.net>

--- drivers/gpu/drm/apple/dcp-internal.h
+++ drivers/gpu/drm/apple/dcp-internal.h
@@ -16,6 +16,12 @@
 
 struct apple_dcp;
 
+enum dcp_firmware_version {
+	DCP_FIRMWARE_UNKNOWN,
+	DCP_FIRMWARE_V_12_3,
+	DCP_FIRMWARE_V_12_4,
+};
+
 enum {
 	SYSTEM_ENDPOINT = 0x20,
 	TEST_ENDPOINT = 0x21,
@@ -83,6 +89,9 @@ struct apple_dcp {
 	struct apple_rtkit *rtk;
 	struct apple_crtc *crtc;
 	struct apple_connector *connector;
+
+	/* firmware version and compatible firmware version */
+	enum dcp_firmware_version fw_version, fw_compat;
 
 	/* Coprocessor control register */
 	void __iomem *coproc_reg;
