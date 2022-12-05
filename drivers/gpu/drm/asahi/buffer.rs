chadmed/linux

drm/asahi: file: Add a ualloc_priv for FW-private user allocs
Not actually private yet because we need to implement the secure unmap
path...

Signed-off-by: Asahi Lina <lina@asahilina.net>

drivers/gpu/drm/asahi/buffer.rs
@@ -112,6 +112,7 @@ impl Buffer::ver {
     pub(crate) fn new(
         alloc: &mut gpu::KernelAllocators,
         ualloc: Ref<Mutex<alloc::SimpleAllocator>>,
+        ualloc_priv: Ref<Mutex<alloc::SimpleAllocator>>,
         mgr: &BufferManager,
     ) -> Result<Buffer::ver> {
         let max_pages = MAX_SIZE / PAGE_SIZE;
@@ -120,8 +121,8 @@ impl Buffer::ver {
         let inner = box_in_place!(buffer::Info::ver {
             block_ctl: alloc.shared.new_default::<buffer::BlockControl>()?,
             counter: alloc.shared.new_default::<buffer::Counter>()?,
-            page_list: ualloc.lock().array_empty(max_pages)?,
-            block_list: ualloc.lock().array_empty(max_blocks)?,
+            page_list: ualloc_priv.lock().array_empty(max_pages)?,
+            block_list: ualloc_priv.lock().array_empty(max_blocks)?,
         })?;
 
         let info = alloc.shared.new_boxed(inner, |inner, ptr| {

drivers/gpu/drm/asahi/file.rs
@@ -7,6 +7,7 @@
 //! Asahi File state
 
 use crate::driver::AsahiDevice;
+use crate::fw::types::*;
 use crate::{alloc, buffer, driver, gem, gpu, mmu, render};
 use kernel::drm::gem::BaseObject;
 use kernel::prelude::*;
@@ -17,6 +18,9 @@ pub(crate) struct File {
     id: u64,
     vm: mmu::Vm,
     ualloc: Ref<Mutex<alloc::SimpleAllocator>>,
+    ualloc_priv: Ref<Mutex<alloc::SimpleAllocator>>,
+    ualloc_extra: alloc::SimpleAllocator,
+    unk_page: GpuArray<u8>,
     renderer: Box<dyn render::Renderer>,
 }
 
@@ -35,16 +39,39 @@ impl drm::file::DriverFile for File {
             &vm,
             0x60_00000000,
             0x60_ffffffff,
+            mmu::PROT_GPU_SHARED_RW,
+            buffer::PAGE_SIZE,
+        )))?;
+        let ualloc_priv = Ref::try_new(Mutex::new(alloc::SimpleAllocator::new_with_range(
+            device,
+            &vm,
+            0x61_00000000,
+            0x61_ffffffff,
             mmu::PROT_GPU_FW_SHARED_RW,
             buffer::PAGE_SIZE,
         )))?;
-        let renderer = device.data().gpu.new_renderer(ualloc.clone())?;
+        let mut ualloc_extra = alloc::SimpleAllocator::new_with_range(
+            device,
+            &vm,
+            0x6f_ffff8000,
+            0x70_00000000,
+            mmu::PROT_GPU_SHARED_RW,
+            0x4000,
+        );
+        let unk_page: GpuArray<u8> = ualloc_extra.array_empty(1)?;
+        let renderer = device
+            .data()
+            .gpu
+            .new_renderer(ualloc.clone(), ualloc_priv.clone())?;
 
         dev_info!(device, "[File {}]: Opened successfully", id);
         Ok(Box::try_new(Self {
             id,
             vm,
             ualloc,
+            ualloc_priv,
+            ualloc_extra,
+            unk_page,
             renderer,
         })?)
     }

drivers/gpu/drm/asahi/gpu.rs
@@ -116,6 +116,7 @@ pub(crate) trait GpuManager: Send + Sync {
     fn new_renderer(
         &self,
         ualloc: Ref<Mutex<alloc::SimpleAllocator>>,
+        ualloc_priv: Ref<Mutex<alloc::SimpleAllocator>>,
     ) -> Result<Box<dyn render::Renderer>>;
     fn submit_batch(&self, batch: workqueue::WorkQueueBatch<'_>) -> Result;
     fn ids(&self) -> &SequenceIDs;
@@ -409,6 +410,7 @@ impl GpuManager for GpuManager::ver {
     fn new_renderer(
         &self,
         ualloc: Ref<Mutex<alloc::SimpleAllocator>>,
+        ualloc_priv: Ref<Mutex<alloc::SimpleAllocator>>,
     ) -> Result<Box<dyn render::Renderer>> {
         let mut kalloc = self.alloc();
         let id = self.ids.renderer.next();
@@ -416,6 +418,7 @@ impl GpuManager for GpuManager::ver {
             &self.dev,
             &mut *kalloc,
             ualloc,
+            ualloc_priv,
             self.event_manager.clone(),
             &self.buffer_mgr,
             id,

drivers/gpu/drm/asahi/render.rs
@@ -62,11 +62,12 @@ impl Renderer::ver {
         dev: &AsahiDevice,
         alloc: &mut gpu::KernelAllocators,
         ualloc: Ref<Mutex<alloc::SimpleAllocator>>,
+        ualloc_priv: Ref<Mutex<alloc::SimpleAllocator>>,
         event_manager: Ref<event::EventManager>,
         mgr: &buffer::BufferManager,
         id: u64,
     ) -> Result<Renderer::ver> {
-        let mut buffer = buffer::Buffer::ver::new(alloc, ualloc, mgr)?;
+        let mut buffer = buffer::Buffer::ver::new(alloc, ualloc, ualloc_priv, mgr)?;
 
         buffer.add_blocks(0x10)?;
 

perjahn/asahilinux

drivers/gpu/drm/asahi/buffer.rs
@@ -112,6 +112,7 @@ impl Buffer::ver {
     pub(crate) fn new(
         alloc: &mut gpu::KernelAllocators,
         ualloc: Arc<Mutex<alloc::SimpleAllocator>>,
+        ualloc_priv: Arc<Mutex<alloc::SimpleAllocator>>,
         mgr: &BufferManager,
     ) -> Result<Buffer::ver> {
         let max_pages = MAX_SIZE / PAGE_SIZE;
@@ -120,8 +121,8 @@ impl Buffer::ver {
         let inner = box_in_place!(buffer::Info::ver {
             block_ctl: alloc.shared.new_default::<buffer::BlockControl>()?,
             counter: alloc.shared.new_default::<buffer::Counter>()?,
-            page_list: ualloc.lock().array_empty(max_pages)?,
-            block_list: ualloc.lock().array_empty(max_blocks)?,
+            page_list: ualloc_priv.lock().array_empty(max_pages)?,
+            block_list: ualloc_priv.lock().array_empty(max_blocks)?,
         })?;
 
         let info = alloc.shared.new_boxed(inner, |inner, ptr| {

drivers/gpu/drm/asahi/file.rs
@@ -7,6 +7,7 @@
 //! Asahi File state
 
 use crate::driver::AsahiDevice;
+use crate::fw::types::*;
 use crate::{alloc, buffer, driver, gem, gpu, mmu, render};
 use kernel::drm::gem::BaseObject;
 use kernel::prelude::*;
@@ -17,6 +18,9 @@ pub(crate) struct File {
     id: u64,
     vm: mmu::Vm,
     ualloc: Arc<Mutex<alloc::SimpleAllocator>>,
+    ualloc_priv: Arc<Mutex<alloc::SimpleAllocator>>,
+    ualloc_extra: alloc::SimpleAllocator,
+    unk_page: GpuArray<u8>,
     renderer: Box<dyn render::Renderer>,
 }
 
@@ -35,16 +39,39 @@ impl drm::file::DriverFile for File {
             &vm,
             0x60_00000000,
             0x60_ffffffff,
+            mmu::PROT_GPU_SHARED_RW,
+            buffer::PAGE_SIZE,
+        )))?;
+        let ualloc_priv = Arc::try_new(Mutex::new(alloc::SimpleAllocator::new_with_range(
+            device,
+            &vm,
+            0x61_00000000,
+            0x61_ffffffff,
             mmu::PROT_GPU_FW_SHARED_RW,
             buffer::PAGE_SIZE,
         )))?;
-        let renderer = device.data().gpu.new_renderer(ualloc.clone())?;
+        let mut ualloc_extra = alloc::SimpleAllocator::new_with_range(
+            device,
+            &vm,
+            0x6f_ffff8000,
+            0x70_00000000,
+            mmu::PROT_GPU_SHARED_RW,
+            0x4000,
+        );
+        let unk_page: GpuArray<u8> = ualloc_extra.array_empty(1)?;
+        let renderer = device
+            .data()
+            .gpu
+            .new_renderer(ualloc.clone(), ualloc_priv.clone())?;
 
         dev_info!(device, "[File {}]: Opened successfully", id);
         Ok(Box::try_new(Self {
             id,
             vm,
             ualloc,
+            ualloc_priv,
+            ualloc_extra,
+            unk_page,
             renderer,
         })?)
     }

drivers/gpu/drm/asahi/gpu.rs
@@ -116,6 +116,7 @@ pub(crate) trait GpuManager: Send + Sync {
     fn new_renderer(
         &self,
         ualloc: Arc<Mutex<alloc::SimpleAllocator>>,
+        ualloc_priv: Arc<Mutex<alloc::SimpleAllocator>>,
     ) -> Result<Box<dyn render::Renderer>>;
     fn submit_batch(&self, batch: workqueue::WorkQueueBatch<'_>) -> Result;
     fn ids(&self) -> &SequenceIDs;
@@ -409,6 +410,7 @@ impl GpuManager for GpuManager::ver {
     fn new_renderer(
         &self,
         ualloc: Arc<Mutex<alloc::SimpleAllocator>>,
+        ualloc_priv: Arc<Mutex<alloc::SimpleAllocator>>,
     ) -> Result<Box<dyn render::Renderer>> {
         let mut kalloc = self.alloc();
         let id = self.ids.renderer.next();
@@ -416,6 +418,7 @@ impl GpuManager for GpuManager::ver {
             &self.dev,
             &mut *kalloc,
             ualloc,
+            ualloc_priv,
             self.event_manager.clone(),
             &self.buffer_mgr,
             id,

drivers/gpu/drm/asahi/render.rs
@@ -62,11 +62,12 @@ impl Renderer::ver {
         dev: &AsahiDevice,
         alloc: &mut gpu::KernelAllocators,
         ualloc: Arc<Mutex<alloc::SimpleAllocator>>,
+        ualloc_priv: Arc<Mutex<alloc::SimpleAllocator>>,
         event_manager: Arc<event::EventManager>,
         mgr: &buffer::BufferManager,
         id: u64,
     ) -> Result<Renderer::ver> {
-        let mut buffer = buffer::Buffer::ver::new(alloc, ualloc, mgr)?;
+        let mut buffer = buffer::Buffer::ver::new(alloc, ualloc, ualloc_priv, mgr)?;
 
         buffer.add_blocks(0x10)?;
 