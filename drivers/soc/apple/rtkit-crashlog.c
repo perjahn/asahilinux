lfnpcsoft/linux-asahi

soc: apple: rtkit: Add register dump decoding to crashlog
Signed-off-by: Asahi Lina <lina@asahilina.net>

drivers/soc/apple/rtkit-crashlog.c
@@ -3,6 +3,7 @@
  * Apple RTKit IPC library
  * Copyright (C) The Asahi Linux Contributors
  */
+#include <asm/esr.h>
 #include "rtkit-internal.h"
 
 #define FOURCC(a, b, c, d) \
@@ -13,6 +14,7 @@
 #define APPLE_RTKIT_CRASHLOG_VERSION FOURCC('C', 'v', 'e', 'r')
 #define APPLE_RTKIT_CRASHLOG_MBOX FOURCC('C', 'm', 'b', 'x')
 #define APPLE_RTKIT_CRASHLOG_TIME FOURCC('C', 't', 'i', 'm')
+#define APPLE_RTKIT_CRASHLOG_REGS FOURCC('C', 'r', 'g', '8')
 
 struct apple_rtkit_crashlog_header {
 	u32 fourcc;
@@ -30,6 +32,24 @@ struct apple_rtkit_crashlog_mbox_entry {
 	u8 _unk[4];
 };
 static_assert(sizeof(struct apple_rtkit_crashlog_mbox_entry) == 0x18);
+
+struct apple_rtkit_crashlog_regs {
+    u32 unk_0;
+    u32 unk_4;
+    u64 regs[31];
+    u64 sp;
+    u64 pc;
+    u64 psr;
+    u64 cpacr;
+    u64 fpsr;
+    u64 fpcr;
+    u64 unk[64];
+    u64 far;
+    u64 unk_X;
+    u64 esr;
+    u64 unk_Z;
+};
+static_assert(sizeof(struct apple_rtkit_crashlog_regs) == 0x350);
 
 static void apple_rtkit_crashlog_dump_str(struct apple_rtkit *rtk, u8 *bfr,
 					  size_t size)
@@ -94,6 +114,73 @@ static void apple_rtkit_crashlog_dump_mailbox(struct apple_rtkit *rtk, u8 *bfr,
 	}
 }
 
+static void apple_rtkit_crashlog_dump_regs(struct apple_rtkit *rtk, u8 *bfr,
+					   size_t size)
+{
+	struct apple_rtkit_crashlog_regs regs;
+	const char *el;
+	int i;
+
+	if (size < sizeof(regs)) {
+		dev_warn(rtk->dev, "RTKit: Regs section too small: 0x%llx", size);
+		return;
+	}
+
+	memcpy(&regs, bfr, sizeof(regs));
+
+	switch (regs.psr & PSR_MODE_MASK) {
+		case PSR_MODE_EL0t:
+			el = "EL0t";
+			break;
+		case PSR_MODE_EL1t:
+			el = "EL1t";
+			break;
+		case PSR_MODE_EL1h:
+			el = "EL1h";
+			break;
+		case PSR_MODE_EL2t:
+			el = "EL2t";
+			break;
+		case PSR_MODE_EL2h:
+			el = "EL2h";
+			break;
+		default:
+			el = "unknown";
+			break;
+	}
+
+	dev_warn(rtk->dev, "RTKit: Exception dump:");
+	dev_warn(rtk->dev, "  == Exception taken from %s ==", el);
+	dev_warn(rtk->dev, "  PSR    = 0x%llx", regs.psr);
+	dev_warn(rtk->dev, "  PC     = 0x%llx\n", regs.pc);
+	dev_warn(rtk->dev, "  ESR    = 0x%llx (%s)\n", regs.esr,
+			 esr_get_class_string(regs.esr));
+	dev_warn(rtk->dev, "  FAR    = 0x%llx\n", regs.far);
+	dev_warn(rtk->dev, "  SP     = 0x%llx\n", regs.sp);
+	dev_warn(rtk->dev, "\n");
+
+	for (i = 0; i < 31; i += 4) {
+		if (i < 28)
+			dev_warn(rtk->dev,
+					 "  x%02d-x%02d = %016llx %016llx %016llx %016llx\n",
+					 i, i + 3,
+					 regs.regs[i], regs.regs[i + 1],
+					 regs.regs[i + 2], regs.regs[i + 3]);
+		else
+			dev_warn(rtk->dev,
+					 "  x%02d-x%02d = %016llx %016llx %016llx\n", i, i + 3,
+					 regs.regs[i], regs.regs[i + 1], regs.regs[i + 2]);
+	}
+
+	dev_warn(rtk->dev, "\n");
+
+	if (esr_is_data_abort(regs.esr)) {
+		dev_warn(rtk->dev, "  Exception was a data abort\n");
+	}
+
+	dev_warn(rtk->dev, "\n");
+}
+
 void apple_rtkit_crashlog_dump(struct apple_rtkit *rtk, u8 *bfr, size_t size)
 {
 	size_t offset;
@@ -138,6 +225,10 @@ void apple_rtkit_crashlog_dump(struct apple_rtkit *rtk, u8 *bfr, size_t size)
 			break;
 		case APPLE_RTKIT_CRASHLOG_TIME:
 			apple_rtkit_crashlog_dump_time(rtk, bfr + offset + 16,
+						       section_size);
+			break;
+		case APPLE_RTKIT_CRASHLOG_REGS:
+			apple_rtkit_crashlog_dump_regs(rtk, bfr + offset + 16,
 						       section_size);
 			break;
 		default:

perjahn/asahilinux

drivers/soc/apple/rtkit-crashlog.c
@@ -13,6 +13,7 @@
 #define APPLE_RTKIT_CRASHLOG_VERSION FOURCC('C', 'v', 'e', 'r')
 #define APPLE_RTKIT_CRASHLOG_MBOX FOURCC('C', 'm', 'b', 'x')
 #define APPLE_RTKIT_CRASHLOG_TIME FOURCC('C', 't', 'i', 'm')
+#define APPLE_RTKIT_CRASHLOG_REGS FOURCC('C', 'r', 'g', '8')
 
 struct apple_rtkit_crashlog_header {
 	u32 fourcc;
@@ -30,6 +31,24 @@ struct apple_rtkit_crashlog_mbox_entry {
 	u8 _unk[4];
 };
 static_assert(sizeof(struct apple_rtkit_crashlog_mbox_entry) == 0x18);
+
+struct apple_rtkit_crashlog_regs {
+    u32 unk_0;
+    u32 unk_4;
+    u64 regs[31];
+    u64 sp;
+    u64 pc;
+    u64 psr;
+    u64 cpacr;
+    u64 fpsr;
+    u64 fpcr;
+    u64 unk[64];
+    u64 far;
+    u64 unk_X;
+    u64 esr;
+    u64 unk_Z;
+};
+static_assert(sizeof(struct apple_rtkit_crashlog_regs) == 0x350);
 
 static void apple_rtkit_crashlog_dump_str(struct apple_rtkit *rtk, u8 *bfr,
 					  size_t size)
@@ -94,6 +113,66 @@ static void apple_rtkit_crashlog_dump_mailbox(struct apple_rtkit *rtk, u8 *bfr,
 	}
 }
 
+static void apple_rtkit_crashlog_dump_regs(struct apple_rtkit *rtk, u8 *bfr,
+					   size_t size)
+{
+	struct apple_rtkit_crashlog_regs regs;
+	const char *el;
+	int i;
+
+	if (size < sizeof(regs)) {
+		dev_warn(rtk->dev, "RTKit: Regs section too small: 0x%lx", size);
+		return;
+	}
+
+	memcpy(&regs, bfr, sizeof(regs));
+
+	switch (regs.psr & PSR_MODE_MASK) {
+		case PSR_MODE_EL0t:
+			el = "EL0t";
+			break;
+		case PSR_MODE_EL1t:
+			el = "EL1t";
+			break;
+		case PSR_MODE_EL1h:
+			el = "EL1h";
+			break;
+		case PSR_MODE_EL2t:
+			el = "EL2t";
+			break;
+		case PSR_MODE_EL2h:
+			el = "EL2h";
+			break;
+		default:
+			el = "unknown";
+			break;
+	}
+
+	dev_warn(rtk->dev, "RTKit: Exception dump:");
+	dev_warn(rtk->dev, "  == Exception taken from %s ==", el);
+	dev_warn(rtk->dev, "  PSR    = 0x%llx", regs.psr);
+	dev_warn(rtk->dev, "  PC     = 0x%llx\n", regs.pc);
+	dev_warn(rtk->dev, "  ESR    = 0x%llx\n", regs.esr);
+	dev_warn(rtk->dev, "  FAR    = 0x%llx\n", regs.far);
+	dev_warn(rtk->dev, "  SP     = 0x%llx\n", regs.sp);
+	dev_warn(rtk->dev, "\n");
+
+	for (i = 0; i < 31; i += 4) {
+		if (i < 28)
+			dev_warn(rtk->dev,
+					 "  x%02d-x%02d = %016llx %016llx %016llx %016llx\n",
+					 i, i + 3,
+					 regs.regs[i], regs.regs[i + 1],
+					 regs.regs[i + 2], regs.regs[i + 3]);
+		else
+			dev_warn(rtk->dev,
+					 "  x%02d-x%02d = %016llx %016llx %016llx\n", i, i + 3,
+					 regs.regs[i], regs.regs[i + 1], regs.regs[i + 2]);
+	}
+
+	dev_warn(rtk->dev, "\n");
+}
+
 void apple_rtkit_crashlog_dump(struct apple_rtkit *rtk, u8 *bfr, size_t size)
 {
 	size_t offset;
@@ -138,6 +217,10 @@ void apple_rtkit_crashlog_dump(struct apple_rtkit *rtk, u8 *bfr, size_t size)
 			break;
 		case APPLE_RTKIT_CRASHLOG_TIME:
 			apple_rtkit_crashlog_dump_time(rtk, bfr + offset + 16,
+						       section_size);
+			break;
+		case APPLE_RTKIT_CRASHLOG_REGS:
+			apple_rtkit_crashlog_dump_regs(rtk, bfr + offset + 16,
 						       section_size);
 			break;
 		default: