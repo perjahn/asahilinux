lfnpcsoft/linux-asahi

drm/asahi: Implement TX channels (DeviceControl & Pipes)
Signed-off-by: Asahi Lina <lina@asahilina.net>

drivers/gpu/drm/asahi/channel.rs
@@ -8,8 +8,8 @@ use crate::fw::channels::*;
 use crate::fw::initdata::{raw, ChannelRing};
 use crate::fw::types::*;
 use crate::gpu;
-use crate::object::GPUStruct;
-use kernel::{dbg, prelude::*};
+use core::time::Duration;
+use kernel::{dbg, delay::coarse_sleep, prelude::*};
 
 pub(crate) struct RXChannel<T: RXChannelState, U: Copy + Default>
 where
@@ -50,6 +50,88 @@ where
                 Some(msg)
             }
         })
+    }
+}
+
+pub(crate) struct TXChannel<T: TXChannelState, U: Copy + Default>
+where
+    for<'a> <T as GPUStruct>::Raw<'a>: Debug + Default,
+{
+    ring: ChannelRing<T, U>,
+    wptr: u32,
+    count: u32,
+}
+
+impl<T: TXChannelState, U: Copy + Default> TXChannel<T, U>
+where
+    for<'a> <T as GPUStruct>::Raw<'a>: Debug + Default,
+{
+    pub(crate) fn new(alloc: &mut gpu::KernelAllocators, count: usize) -> Result<TXChannel<T, U>> {
+        Ok(TXChannel {
+            ring: ChannelRing {
+                state: alloc.shared.new_default()?,
+                ring: alloc.private.array_empty(count)?,
+            },
+            wptr: 0,
+            count: count as u32,
+        })
+    }
+
+    pub(crate) fn put(&mut self, msg: &U) {
+        self.ring.state.with(|raw, _inner| {
+            let next_wptr = (self.wptr + 1) % self.count;
+            let mut rptr = T::rptr(raw);
+            if next_wptr == rptr {
+                pr_err!("TX ring buffer is full! Waiting...");
+                // TODO: block properly on incoming messages?
+                while next_wptr == rptr {
+                    coarse_sleep(Duration::from_millis(8));
+                    rptr = T::rptr(raw);
+                }
+            }
+            self.ring.ring.as_mut_slice()[self.wptr as usize] = *msg;
+            T::set_wptr(raw, next_wptr);
+        })
+    }
+}
+
+pub(crate) struct DeviceControlChannel {
+    ch: TXChannel<ChannelState, DeviceControlMsg>,
+}
+
+impl DeviceControlChannel {
+    pub(crate) fn new(alloc: &mut gpu::KernelAllocators) -> Result<DeviceControlChannel> {
+        Ok(DeviceControlChannel {
+            ch: TXChannel::<ChannelState, DeviceControlMsg>::new(alloc, 0x100)?,
+        })
+    }
+
+    pub(crate) fn to_raw(&self) -> raw::ChannelRing<ChannelState, DeviceControlMsg> {
+        self.ch.ring.to_raw()
+    }
+
+    pub(crate) fn send(&mut self, msg: &DeviceControlMsg) {
+        self.ch.put(msg);
+    }
+}
+
+pub(crate) struct PipeChannel {
+    ch: TXChannel<ChannelState, PipeMsg>,
+}
+
+impl PipeChannel {
+    pub(crate) fn new(alloc: &mut gpu::KernelAllocators) -> Result<PipeChannel> {
+        Ok(PipeChannel {
+            ch: TXChannel::<ChannelState, PipeMsg>::new(alloc, 0x100)?,
+        })
+    }
+
+    pub(crate) fn to_raw(&self) -> raw::ChannelRing<ChannelState, PipeMsg> {
+        self.ch.ring.to_raw()
+    }
+
+    pub(crate) fn send(&mut self, msg: &PipeMsg) {
+        self.ch.put(msg);
     }
 }
 

drivers/gpu/drm/asahi/fw/channels.rs
@@ -87,8 +87,70 @@ impl GPUStruct for FWCtlChannelState {
     type Raw<'a> = raw::FWCtlChannelState<'a>;
 }
 
-pub(crate) type RunCmdQueueMsg = Array<0x30, u8>;
-pub(crate) type DeviceControlMsg = Array<0x30, u8>;
+pub(crate) trait TXChannelState: GPUStruct + Debug + Default {
+    fn rptr<'a>(raw: &Self::Raw<'a>) -> u32;
+    fn set_wptr<'a>(raw: &Self::Raw<'a>, wptr: u32);
+}
+
+impl TXChannelState for ChannelState {
+    fn rptr<'a>(raw: &Self::Raw<'a>) -> u32 {
+        raw.read_ptr.load(Ordering::Acquire)
+    }
+
+    fn set_wptr<'a>(raw: &Self::Raw<'a>, wptr: u32) {
+        raw.write_ptr.store(wptr, Ordering::Release);
+    }
+}
+
+impl TXChannelState for FWCtlChannelState {
+    fn rptr<'a>(raw: &Self::Raw<'a>) -> u32 {
+        raw.read_ptr.load(Ordering::Acquire)
+    }
+
+    fn set_wptr<'a>(raw: &Self::Raw<'a>, wptr: u32) {
+        raw.write_ptr.store(wptr, Ordering::Release);
+    }
+}
+
+pub(crate) type PipeMsg = Array<0x30, u8>;
+
+pub(crate) const DEVICECONTROL_SZ: usize = 0x30;
+
+// TODO: clean up when arbitrary_enum_discriminant is stable
+// https://github.com/rust-lang/rust/issues/60553
+
+#[derive(Debug, Copy, Clone)]
+#[repr(C, u32)]
+pub(crate) enum DeviceControlMsg {
+    Unk00(Array<DEVICECONTROL_SZ, u8>),
+    Unk01(Array<DEVICECONTROL_SZ, u8>),
+    Unk02(Array<DEVICECONTROL_SZ, u8>),
+    Unk03(Array<DEVICECONTROL_SZ, u8>),
+    Unk04(Array<DEVICECONTROL_SZ, u8>),
+    Unk05(Array<DEVICECONTROL_SZ, u8>),
+    Unk06(Array<DEVICECONTROL_SZ, u8>),
+    Unk07(Array<DEVICECONTROL_SZ, u8>),
+    Unk08(Array<DEVICECONTROL_SZ, u8>),
+    Unk09(Array<DEVICECONTROL_SZ, u8>),
+    Unk0a(Array<DEVICECONTROL_SZ, u8>),
+    Unk0b(Array<DEVICECONTROL_SZ, u8>),
+    Unk0c(Array<DEVICECONTROL_SZ, u8>),
+    Unk0d(Array<DEVICECONTROL_SZ, u8>),
+    Unk0e(Array<DEVICECONTROL_SZ, u8>),
+    Unk0f(Array<DEVICECONTROL_SZ, u8>),
+    Unk10(Array<DEVICECONTROL_SZ, u8>),
+    Unk11(Array<DEVICECONTROL_SZ, u8>),
+    Unk12(Array<DEVICECONTROL_SZ, u8>),
+    Unk13(Array<DEVICECONTROL_SZ, u8>),
+    Unk14(Array<DEVICECONTROL_SZ, u8>),
+    Unk15(Array<DEVICECONTROL_SZ, u8>),
+    Unk16(Array<DEVICECONTROL_SZ, u8>),
+    Unk17(Array<DEVICECONTROL_SZ, u8>),
+    Unk18(Array<DEVICECONTROL_SZ, u8>),
+    Initialize,
+}
+default_zeroed!(DeviceControlMsg);
+
 pub(crate) type RawEventMsg = Array<0x38, u8>;
 pub(crate) type RawFWLogMsg = Array<0xd8, u8>;
 pub(crate) type FWCtlMsg = Array<0x14, u8>;

perjahn/asahilinux

drivers/gpu/drm/asahi/channel.rs
@@ -8,8 +8,8 @@ use crate::fw::channels::*;
 use crate::fw::initdata::{raw, ChannelRing};
 use crate::fw::types::*;
 use crate::gpu;
-use crate::object::GpuStruct;
-use kernel::{dbg, prelude::*};
+use core::time::Duration;
+use kernel::{dbg, delay::coarse_sleep, prelude::*};
 
 pub(crate) struct RxChannel<T: RxChannelState, U: Copy + Default>
 where
@@ -50,6 +50,88 @@ where
                 Some(msg)
             }
         })
+    }
+}
+
+pub(crate) struct TxChannel<T: TxChannelState, U: Copy + Default>
+where
+    for<'a> <T as GpuStruct>::Raw<'a>: Debug + Default,
+{
+    ring: ChannelRing<T, U>,
+    wptr: u32,
+    count: u32,
+}
+
+impl<T: TxChannelState, U: Copy + Default> TxChannel<T, U>
+where
+    for<'a> <T as GpuStruct>::Raw<'a>: Debug + Default,
+{
+    pub(crate) fn new(alloc: &mut gpu::KernelAllocators, count: usize) -> Result<TxChannel<T, U>> {
+        Ok(TxChannel {
+            ring: ChannelRing {
+                state: alloc.shared.new_default()?,
+                ring: alloc.private.array_empty(count)?,
+            },
+            wptr: 0,
+            count: count as u32,
+        })
+    }
+
+    pub(crate) fn put(&mut self, msg: &U) {
+        self.ring.state.with(|raw, _inner| {
+            let next_wptr = (self.wptr + 1) % self.count;
+            let mut rptr = T::rptr(raw);
+            if next_wptr == rptr {
+                pr_err!("TX ring buffer is full! Waiting...");
+                // TODO: block properly on incoming messages?
+                while next_wptr == rptr {
+                    coarse_sleep(Duration::from_millis(8));
+                    rptr = T::rptr(raw);
+                }
+            }
+            self.ring.ring.as_mut_slice()[self.wptr as usize] = *msg;
+            T::set_wptr(raw, next_wptr);
+        })
+    }
+}
+
+pub(crate) struct DeviceControlChannel {
+    ch: TxChannel<ChannelState, DeviceControlMsg>,
+}
+
+impl DeviceControlChannel {
+    pub(crate) fn new(alloc: &mut gpu::KernelAllocators) -> Result<DeviceControlChannel> {
+        Ok(DeviceControlChannel {
+            ch: TxChannel::<ChannelState, DeviceControlMsg>::new(alloc, 0x100)?,
+        })
+    }
+
+    pub(crate) fn to_raw(&self) -> raw::ChannelRing<ChannelState, DeviceControlMsg> {
+        self.ch.ring.to_raw()
+    }
+
+    pub(crate) fn send(&mut self, msg: &DeviceControlMsg) {
+        self.ch.put(msg);
+    }
+}
+
+pub(crate) struct PipeChannel {
+    ch: TxChannel<ChannelState, PipeMsg>,
+}
+
+impl PipeChannel {
+    pub(crate) fn new(alloc: &mut gpu::KernelAllocators) -> Result<PipeChannel> {
+        Ok(PipeChannel {
+            ch: TxChannel::<ChannelState, PipeMsg>::new(alloc, 0x100)?,
+        })
+    }
+
+    pub(crate) fn to_raw(&self) -> raw::ChannelRing<ChannelState, PipeMsg> {
+        self.ch.ring.to_raw()
+    }
+
+    pub(crate) fn send(&mut self, msg: &PipeMsg) {
+        self.ch.put(msg);
     }
 }
 

drivers/gpu/drm/asahi/fw/channels.rs
@@ -87,8 +87,70 @@ impl GpuStruct for FwCtlChannelState {
     type Raw<'a> = raw::FwCtlChannelState<'a>;
 }
 
-pub(crate) type RunCmdQueueMsg = Array<0x30, u8>;
-pub(crate) type DeviceControlMsg = Array<0x30, u8>;
+pub(crate) trait TxChannelState: GpuStruct + Debug + Default {
+    fn rptr(raw: &Self::Raw<'_>) -> u32;
+    fn set_wptr(raw: &Self::Raw<'_>, wptr: u32);
+}
+
+impl TxChannelState for ChannelState {
+    fn rptr(raw: &Self::Raw<'_>) -> u32 {
+        raw.read_ptr.load(Ordering::Acquire)
+    }
+
+    fn set_wptr(raw: &Self::Raw<'_>, wptr: u32) {
+        raw.write_ptr.store(wptr, Ordering::Release);
+    }
+}
+
+impl TxChannelState for FwCtlChannelState {
+    fn rptr(raw: &Self::Raw<'_>) -> u32 {
+        raw.read_ptr.load(Ordering::Acquire)
+    }
+
+    fn set_wptr(raw: &Self::Raw<'_>, wptr: u32) {
+        raw.write_ptr.store(wptr, Ordering::Release);
+    }
+}
+
+pub(crate) type PipeMsg = Array<0x30, u8>;
+
+pub(crate) const DEVICECONTROL_SZ: usize = 0x30;
+
+// TODO: clean up when arbitrary_enum_discriminant is stable
+// https://github.com/rust-lang/rust/issues/60553
+
+#[derive(Debug, Copy, Clone)]
+#[repr(C, u32)]
+pub(crate) enum DeviceControlMsg {
+    Unk00(Array<DEVICECONTROL_SZ, u8>),
+    Unk01(Array<DEVICECONTROL_SZ, u8>),
+    Unk02(Array<DEVICECONTROL_SZ, u8>),
+    Unk03(Array<DEVICECONTROL_SZ, u8>),
+    Unk04(Array<DEVICECONTROL_SZ, u8>),
+    Unk05(Array<DEVICECONTROL_SZ, u8>),
+    Unk06(Array<DEVICECONTROL_SZ, u8>),
+    Unk07(Array<DEVICECONTROL_SZ, u8>),
+    Unk08(Array<DEVICECONTROL_SZ, u8>),
+    Unk09(Array<DEVICECONTROL_SZ, u8>),
+    Unk0a(Array<DEVICECONTROL_SZ, u8>),
+    Unk0b(Array<DEVICECONTROL_SZ, u8>),
+    Unk0c(Array<DEVICECONTROL_SZ, u8>),
+    Unk0d(Array<DEVICECONTROL_SZ, u8>),
+    Unk0e(Array<DEVICECONTROL_SZ, u8>),
+    Unk0f(Array<DEVICECONTROL_SZ, u8>),
+    Unk10(Array<DEVICECONTROL_SZ, u8>),
+    Unk11(Array<DEVICECONTROL_SZ, u8>),
+    Unk12(Array<DEVICECONTROL_SZ, u8>),
+    Unk13(Array<DEVICECONTROL_SZ, u8>),
+    Unk14(Array<DEVICECONTROL_SZ, u8>),
+    Unk15(Array<DEVICECONTROL_SZ, u8>),
+    Unk16(Array<DEVICECONTROL_SZ, u8>),
+    Unk17(Array<DEVICECONTROL_SZ, u8>),
+    Unk18(Array<DEVICECONTROL_SZ, u8>),
+    Initialize,
+}
+default_zeroed!(DeviceControlMsg);
+
 pub(crate) type RawEventMsg = Array<0x38, u8>;
 pub(crate) type RawFwLogMsg = Array<0xd8, u8>;
 pub(crate) type FwCtlMsg = Array<0x14, u8>;