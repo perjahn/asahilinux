71977295ebc1e477b05acd0076288f383a4b6e69
Asahi Lina <lina@asahilina.net>
2023-01-25 18:05:56 +0900

    fixup! rust: drm/gem/shmem: Gate module & helpers on DRM_GEM_SHMEM_HELPER

--- rust/kernel/drm/gem/mod.rs
+++ rust/kernel/drm/gem/mod.rs
@@ -5,7 +5,7 @@
 //!
 //! C header: [`include/linux/drm/drm_gem.h`](../../../../include/linux/drm/drm_gem.h)
 
-#[cfg(CONFIG_DRM_GEM_SHMEM_HELPER)]
+#[cfg(CONFIG_DRM_GEM_SHMEM_HELPER = "y")]
 pub mod shmem;
 
 use alloc::boxed::Box;
