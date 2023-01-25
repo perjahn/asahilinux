cbb5cf8f4545755c11641adc3701a1c8e64c7ca0
Asahi Lina <lina@asahilina.net>
2023-01-25 20:26:07 +0900

    drm/asahi: util: Docs
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- drivers/gpu/drm/asahi/util.rs
+++ drivers/gpu/drm/asahi/util.rs
@@ -1,36 +1,40 @@
 // SPDX-License-Identifier: GPL-2.0-only OR MIT
-#![allow(missing_docs)]
 
-//! Utility functions
+//! Miscellaneous utility functions
 
 use core::ops::{Add, BitAnd, Div, Not, Sub};
 
+/// Aligns an integer type to a power of two.
 pub(crate) fn align<T>(a: T, b: T) -> T
 where
-    T: Copy,
-    T: Default,
-    T: BitAnd<Output = T>,
-    T: Not<Output = T>,
-    T: Add<Output = T>,
-    T: Sub<Output = T>,
-    T: Div<Output = T>,
+    T: Copy
+        + Default
+        + BitAnd<Output = T>
+        + Not<Output = T>
+        + Add<Output = T>
+        + Sub<Output = T>
+        + Div<Output = T>
+        + core::cmp::PartialEq,
 {
     let def: T = Default::default();
     #[allow(clippy::eq_op)]
     let one: T = !def / !def;
 
+    assert!((b & (b - one)) == def);
+
     (a + b - one) & !(b - one)
 }
 
+/// Integer division rounding up.
 pub(crate) fn div_ceil<T>(a: T, b: T) -> T
 where
-    T: Copy,
-    T: Default,
-    T: BitAnd<Output = T>,
-    T: Not<Output = T>,
-    T: Add<Output = T>,
-    T: Sub<Output = T>,
-    T: Div<Output = T>,
+    T: Copy
+        + Default
+        + BitAnd<Output = T>
+        + Not<Output = T>
+        + Add<Output = T>
+        + Sub<Output = T>
+        + Div<Output = T>,
 {
     let def: T = Default::default();
     #[allow(clippy::eq_op)]
