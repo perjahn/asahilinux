370a98a46d88da6602ecf2a62b7b7b38c12c28f1
Asahi Lina <lina@asahilina.net>
2023-01-25 16:09:10 +0900

    fixup! rust: time: New module

--- rust/kernel/time.rs
+++ rust/kernel/time.rs
@@ -8,14 +8,12 @@
 use crate::bindings;
 use core::time::Duration;
 
-/// Returns the kernel time elapsed since boot, excluding time spent sleeping,
-/// as a [`Duration`].
+/// Returns the kernel time elapsed since boot, excluding time spent sleeping, as a [`Duration`].
 pub fn ktime_get() -> Duration {
     Duration::from_nanos(unsafe { bindings::ktime_get() }.try_into().unwrap())
 }
 
-/// Returns the kernel time elapsed since boot, including time spent sleeping,
-/// as a [`Duration`].
+/// Returns the kernel time elapsed since boot, including time spent sleeping, as a [`Duration`].
 pub fn ktime_get_boottime() -> Duration {
     Duration::from_nanos(
         unsafe { bindings::ktime_get_with_offset(bindings::tk_offsets_TK_OFFS_BOOT) }
